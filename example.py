89d923f385d33131c5e25e070000a40eb3e310c7
shoufengwei <772920866@qq.com>
2023-05-29 14:39:54 +0800

    Create example.py

---
+++ example.py
@@ -0,0 +1,18 @@
+import time
+import requests
+
+data = {
+    'session': 'uuid v4',
+    'user': 'your token',
+    'question': 'hi'
+}
+response = requests.post('http://openai.yige.space/api/data/', data=data)  # 提问
+if response.status_code == 200:  # 判断是否正常返回
+    id = response.json()['data']['id']  # 获取本次提问 id
+    while True:
+        time.sleep(1)  # 1s 轮询一次，等待结果
+        response = requests.get(f'http://openai.yige.space/api/data/{id}/')  # 查询结果
+        if response.status_code == 200 and response.json()['data']['status'] == 2:  # 正常处理完毕
+            answer = response.json()['data']['answer']  # 取出结果
+            print(answer)
+            break
